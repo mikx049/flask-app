d6b85cc376a6461f9104a7c019ddb24a8f9c956a
mikx049 <mikolajrokita157@gmail.com>
2025-07-12 11:04:25 +0200

    no api

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
-import api
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
-api.main()
 if __name__ == "__main__":
     app.run()
